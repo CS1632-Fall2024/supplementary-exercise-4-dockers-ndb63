c26e92f123c3f8e6dee839a991c5f97e2f05ccca
Nicob1203 <ndb63@pitt.edu>
2024-11-22 19:17:35 -0500

    s

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -35,7 +35,7 @@ public class D3Test {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-    driver = new FirefoxDriver();
+    driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
     ChromeOptions options = new ChromeOptions();
